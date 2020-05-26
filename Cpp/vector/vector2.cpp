cc0cdaed403f1a119abb93f5be5e10349abd1548
summer <56674834+Mrszhao112@users.noreply.github.com>
2020-05-26 13:29:28 +0800

    vector具体实现
    赋值运算符重载现代写法 拷贝构造 reverse resize以及其他vector类功能的具体实现

--- Cpp/vector/vector2.cpp
+++ Cpp/vector/vector2.cpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <assert.h>
-
+#include <iostream>
 namespace my_vector {
 
 	//模板的使用示例
@@ -27,40 +27,59 @@ public:
 	{}
 
 	//构造
-	////v2(v1)  此方法不允许
-	//vector(const vector<T>& v) {
+	////v2(v1) 
+	//vector(const vector<T>& v) { //直接 构造出一个想要的空间 然后逐一赋值 
 	//	_start = new T[v.capacity()]; //指向空间首地址
 	//	_finish = _start;
 	//	_endofstorage = _start + v.capacity(); //指向最大容量处
 
-	//	for (size_t i = 0; i < v.size(); i++) {
+	//	for (size_t i = 0; i < v.size(); i++) { //循环赋值
 	//		*_finish = v[i];
 	//		++_finish;
 	//	}
 	//}
-  
 	//v2(v1)
-	vector(const vector<T>& v) {
+	vector(const vector<T>& v)//先初始化 然后reverse开空间 逐一赋值
 		:_start(nullptr)
-			, _finish(nullptr)
-			, _endofstorage(nullptr)
+		, _finish(nullptr)
+		, _endofstorage(nullptr)
 		{
 			reverse(v.capacity());
 			for (const auto& e : v) {
 				push_back(e);
 			}
 		}
-	}
+		
+
 	//赋值运算符重载 现代写法 
-	vector<T>& operator=(vector<T> v) {
-		swap(v);
+	//vector<T>& operator=(vector<T> v) { //进来的系统自动拷贝一份v
+	//	swap(v); //其实是交换 this 和 v
+	//	return *this;
+	//}
+	///////////////////////////////////////////////////////////////////////////////////
+	vector<T>& operator=(vector<T>& v) {
+		//释放原来的空间
+		delete[] _start;
+		_start = _finish = _endofstorage = nullptr;
+		//开辟新空间
+		T* tmp = new T[v.capacity()];
+		//赋值
+		for (size_t i = 0; i < v.size(); i++) {
+			tmp[i] = v[i];
+		}
+		//结束
+		_start = tmp;
+		_finish = tmp + v.size();
+		_endofstorage = tmp + v.capacity();
 		return *this;
 	}
-	void swap(vector<T>& v) { //因为swap函数创建了临时变量进行存储
+	//////////////////////////////////////////////////////////////////////////////////////
+	void swap(vector<T>& v) { 
 		::swap(_start, v._start);
 		::swap(_finish, v._finish);
 		::swap(_endofstorage, v._endofstorage);
 	}
+
 	~vector() { //析构
 		delete[] _start;
 		_start = _finish = _endofstorage = nullptr;
@@ -81,7 +100,7 @@ public:
 		if (n >capacity()) {
 			size_t sz = size();
 			T* tmp = new T[n];
-			if (_start) {
+			if (_start) { //判断是不是空 如果为空则只需要进行指针的大小变化 无需进行值拷贝
 				//memcpy(tmp, _start, sizeof(T)*sz);浅拷贝 
 				for (size_t i = 0; i < sz; ++i) {
 					tmp[i] = _start[i];
@@ -93,6 +112,7 @@ public:
 			_endofstorage = tmp + n;
 		}
 	}
+
 	void resize(size_t n, const T& val = T()) { //此处为缺省参数
 		if (n < size()) {
 			_finish = _start + n;
@@ -107,14 +127,18 @@ public:
 			}
 		}
 	}
+
 	void push_back(const T& x) {
 		if (_finish == _endofstorage) {
 			size_t newcapacity = capacity() == 0 ? 2 : capacity() * 2;
 			reverse(newcapacity);
 		}
+		/////////////////////////////////////
 		*_finish = x;
 		_finish++;
+		//insert(_finish, x);
 	}
+
 	void pop_back() {
 		assert(_start < _finish);
 		--_finish;
@@ -122,7 +146,7 @@ public:
 	void insert(iterator pos, const T& x) {
 		assert(pos <= _finish);
 		if (_finish == _endofstorage) {
-			size_t n = pos - _start;
+			size_t n = pos - _start;//扩容时保存相对位置
 			size_t newcapacity = capacity() == 0 ? 2 : capacity() * 2;
 			reverse(newcapacity);
 			pos = _start + n;
@@ -135,6 +159,7 @@ public:
 		*pos = x;
 		++_finish;
 	}
+
 	iterator erase(iterator pos) {
 		assert(pos >= _start && pos < _finish);
 		iterator it = pos;
@@ -164,4 +189,35 @@ private:
 	iterator _finish;
 	iterator _endofstorage;
 	};
+
+	//测试
+	//迭代器失效问题
+
+	void print_vector(const vector<int>& v) {
+		vector<int>::const_iterator it = v.begin();
+		while (it != v.end()) {
+			std::cout << *it << " ";
+			++it;
+		}
+		std::cout << std::endl;
+	}
+	void test1() {
+		vector<int> v;
+		v.push_back(1);
+		v.push_back(3);
+		v.push_back(2);
+		print_vector(v);
+
+		//std::cout << v.size() << std::endl;
+		//std::cout << v.capacity() << std::endl;
+		//print_vector(v);
+
+		vector<int> x;
+		x.push_back(100);
+		print_vector(x);
+		x = v;
+		print_vector(x);
+		print_vector(v);
+
+	}
 }
